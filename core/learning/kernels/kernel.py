7c8f2d06c00e51a0af225bc6fe6b6c6362fe91a7
Victor Dorobantu <victor.d.dorobantu@gmail.com>
2020-03-03 11:20:47 -0800

    Added embedding function to kernel class

--- core/learning/kernels/kernel.py
+++ core/learning/kernels/kernel.py
@@ -1,5 +1,7 @@
 from numpy import array
 
+from ...util import arr_map
+
 class Kernel:
     def K(self, x, y):
         raise NotImplementedError
@@ -7,5 +9,13 @@ class Kernel:
     def label(self):
         raise NotImplementedError
 
+    def embedding(self, xs):
+
+        def phi(self, x):
+            return array([self.K(x_i, x) for x_i in xs])
+
+        return phi
+
     def matrix(self, xs):
-         return array([[self.K(x_i, x_j) for x_i in xs] for x_j in xs])
+        phi = self.embedding(xs)
+        return arr_map(phi, xs)
